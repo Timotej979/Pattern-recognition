a7627bc7b9dc567b7b7a05a0de3410b3508b96bb
Timotej979 <timotej.pet@gmail.com>
2022-10-31 12:46:25 +0100

    exercise1.py -> exercise1a.py, workable program

---
+++ Exercise1/exercise1a.py
@@ -0,0 +1,71 @@
+from cv2 import threshold
+import matplotlib.pyplot as plt
+import cv2, sys, logging
+
+from thresholding import DigitalImageProcessing
+
+
+def exercise1A_thresholding():
+    # In Short: It allows you to execute code when the file runs as a script, but not when it's imported as a module
+    try:
+        filename = sys.argv[1]
+        usedFilter = sys.argv[2]
+    except IndexError:
+        logging.error(" Wrong usage of program input arguments\nCorrect program usage: python exercise1.py <Image filename> <Image filter number>\nPossible image filters:\n   0 (original image)\n   1 (equalized histogram)\n   2 (gaussian blur)\n   3 (median blur)\n ")
+        sys.exit(1)
+
+    # Read image and convert to correct colourspace
+    readImage = cv2.imread(filename)
+    readImage = cv2.cvtColor(readImage, cv2.COLOR_BGR2RGB)
+    grayScaleImage = cv2.cvtColor(readImage, cv2.COLOR_RGB2GRAY)
+
+    # Image processing with 
+    #   - histogram equalization
+    #   - median Blur
+    #   - Gaussian Blur
+    if usedFilter == '0':
+        processedImage = grayScaleImage
+    elif usedFilter == '1':
+        processedImage = cv2.equalizeHist(grayScaleImage)
+    elif usedFilter == '2':
+        processedImage = cv2.GaussianBlur(grayScaleImage, (5, 5), cv2.BORDER_DEFAULT)
+    elif usedFilter == '3':
+        processedImage = cv2.medianBlur(grayScaleImage, 9)
+    else:
+        logging.error(" <Image filter number> not valid, choose numbers 0-3")
+
+    # Create new DIP object to calculate image parameters and display them
+    ImageProcessingObj = DigitalImageProcessing(processedImage)
+    ImageProcessingObj.calculateHistogram()
+    ImageProcessingObj.drawHistogram()
+
+    plt.figure()
+    plt.title("RGB image")
+    plt.imshow(readImage)
+
+    plt.figure()
+    plt.title("Grayscale image")
+    plt.imshow(grayScaleImage, cmap = "gray")
+
+    plt.figure()
+    plt.title("Processed image")
+    plt.imshow(processedImage, cmap = "gray")
+
+    # Set image threshold
+    ImageProcessingObj.setThreshold()
+    logging.info("Selected image threshold is: {}".format(ImageProcessingObj.imageThreshold))
+    _, thresholdedImage = cv2.threshold(processedImage, ImageProcessingObj.imageThreshold, 255, 0)
+
+    plt.figure()
+    plt.title("Thresholded image")
+    plt.imshow(thresholdedImage, cmap = "gray")
+    plt.show()
+
+    input("Press any key to exit the program")
+    plt.close("all")
+    sys.exit(0)
+
+
+if __name__ == "__main__":
+    logging.basicConfig(level = logging.INFO)
+    exercise1A_thresholding()
--- Exercise1/thresholding.py
+++ Exercise1/thresholding.py
@@ -1,28 +1,28 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-class DigitalImageProcessing():
+class DigitalImageProcessing:
 
     def __init__(self, grayScaleImage):
         self.grayScaleImage = grayScaleImage
 
     def calculateHistogram(self):
         """Calculate histogram of a grayscale image.
-        @param greyImage : uint8 2D numpy array of variable size
+        @param grayImage : uint8 2D numpy array of variable size
         output: image histogram"""
         histogram = np.zeros((256,), dtype="float64")
         for gray_threshold in range(256):
             pixel_num = np.sum(self.grayScaleImage == gray_threshold)
             histogram[gray_threshold] = pixel_num
-        self.histogram = histogram
+        self.imageHistogram = histogram
 
     def drawHistogram(self):
         """Draws calculated histogram.
         @param histogram : 1D numpy array of variable size and type"""
-        element_num = self.histogram.shape[0]
+        element_num = self.imageHistogram.shape[0]
         plt.figure()
         plt.title("Histogram")
-        plt.bar(np.arange(element_num), self.histogram)
+        plt.bar(np.arange(element_num), self.imageHistogram)
         plt.xlabel("Luminosity")
         plt.ylabel("Number of pixels")
         plt.grid(True)
@@ -30,19 +30,17 @@ class DigitalImageProcessing():
 
     def setThreshold(self):
         """Sets threshold of a grayscale image by maximizing information.
-        @param greyImage : Image we are setting a threshold to
+        @param grayImage : Image we are setting a threshold to
         output: Value of threshold, which maximizes information"""
-        histogram = self.calculateHistogram(self.grayScaleImage)
-
         # Calculate number of pixels in image
         n = self.grayScaleImage.shape[0] * self.grayScaleImage.shape[1]
 
         # Calculate distribution of relative frequencies, P
-        P = histogram / n
+        P = self.imageHistogram / n
 
         # Initialize vector where we save our  information
         # For every possible value of threshold
-        information = np.zeros_like(histogram)
+        information = np.zeros_like(self.imageHistogram)
 
         # Calculate information at every possible value of threshold
         # Set threshold value which maximizes information
@@ -62,4 +60,4 @@ class DigitalImageProcessing():
         # Calculate information at every possible threshold:
         # Search for threshold value where the information is maximized
         t = np.argmax(information)
-        self.threshold = t
+        self.imageThreshold = t
