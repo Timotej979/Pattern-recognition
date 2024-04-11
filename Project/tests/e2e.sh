ddffc675e644df78836b9c743d5a3d75e7d82f59
Timotej979 <timotej.pet@gmail.com>
2024-04-11 14:22:08 +0200

    Fix import(test_dal.py),rm redundant code(e2e.sh)

--- Project/tests/e2e.sh
+++ Project/tests/e2e.sh
@@ -3,14 +3,10 @@
 # Timeout in seconds
 timeout=300  # Adjust the timeout value as needed
 
-# Start the services
-docker compose build
-docker compose up -d
-
 # Function to check if a service is healthy
 is_service_healthy() {
     service_name="$1"
-    docker-compose ps -q "$service_name" | xargs docker inspect --format='{{.State.Health.Status}}' | grep -q 'healthy'
+    docker compose ps -q "$service_name" | xargs docker inspect --format='{{.State.Health.Status}}' | grep -q 'healthy'
 }
 
 # Function to check if the timeout has expired
@@ -21,7 +17,7 @@ is_timeout_expired() {
 start_time=$(date +%s)
 
 # Wait for all services to be healthy within the specified timeout
-for service in $(docker-compose config --services); do
+for service in $(docker compose config --services); do
     until is_service_healthy "$service" || is_timeout_expired; do
         echo "Waiting for $service to be healthy..."
         sleep 1
@@ -35,12 +31,9 @@ for service in $(docker-compose config --services); do
 done
 
 # Check if all services are healthy
-if ! docker-compose ps --services | xargs -n 1 docker inspect --format='{{.State.Health.Status}}' | grep -q 'unhealthy'; then
+if ! docker compose ps --services | xargs -n 1 docker inspect --format='{{.State.Health.Status}}' | grep -q 'unhealthy'; then
     echo "All services are healthy."
 else
     echo "Some services are unhealthy."
     exit 1
 fi
-
-# Clean up Docker containers
-docker compose down
